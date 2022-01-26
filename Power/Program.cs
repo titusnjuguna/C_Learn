d39ab3647b1f69f03893c18b01dc7653f8143e2a
titus njuguna <titusnjuguna59@gmail.com>
2022-01-27 00:03:53 +0300

    Power of a Number

---
+++ Power/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber, powerNumber;
+
+            Console.Write("Enter base number :");
+            baseNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter exponent number :");
+            powerNumber = Convert.ToDouble(Console.ReadLine());
+
+            double returnNumber = Math.Pow(baseNumber, powerNumber);
+            Console.WriteLine($"{baseNumber}^{powerNumber} = {returnNumber}");
+
+            Console.ReadLine();
+        }
+    }
+}
