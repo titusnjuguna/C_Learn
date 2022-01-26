083fdfa6227b70bd41d23416e5679a595c85200e
titus njuguna <titusnjuguna59@gmail.com>
2022-01-26 12:21:00 +0300

    Calculate area of Rectangle

---
+++ AreaRectangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length,width,Area;
+            Console.WriteLine("Enter the length:");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the width:");
+            width = Convert.ToInt32(Console.ReadLine());
+            Area = length * width;
+            Console.WriteLine($"The area of the rectangle is {Area} M squared!");
+        }
+    }
+}
