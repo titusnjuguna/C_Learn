41b9bcef8733871dc8135da6bab75b2457b61fe1
titus njuguna <titusnjuguna59@gmail.com>
2022-01-26 14:54:17 +0300

    Perimeter and area of a square

---
+++ PeriArea_calc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PeriArea_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length,Area,Perimeter;
+            Console.WriteLine("Enter the length of the Square:");
+            length = Convert.ToInt32(Console.ReadLine());
+            Area = length * length ;
+            Perimeter = length * 4;
+            Console.WriteLine($"The Area of the Square is {Area} MSq and the perimeter is {Perimeter} M");
+        }
+    }
+}
