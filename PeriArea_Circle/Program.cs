555eaa2fcba8db78cebcff54eeebf8e3c7bf6e08
titus njuguna <titusnjuguna59@gmail.com>
2022-01-26 15:21:13 +0300

    Perimeter and area of a Circle

---
+++ PeriArea_Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PeriArea_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            double Pi = 3.1456789D,Area, Perimeter;
+            Console.WriteLine("Enter the radius of the Circle:");
+            radius = Convert.ToInt32(Console.ReadLine());
+            Area = 2 * Pi * radius * radius;
+            Perimeter = 2 * Pi * radius;
+            Console.WriteLine($"The Area of the Circle is {Area} MSq and the perimeter is {Perimeter} M");
+            Console.ReadKey();
+        }
+    }
+}
