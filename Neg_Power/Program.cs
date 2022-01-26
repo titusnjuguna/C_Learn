9c599178fd3b9fa4d8a395825a34433ce000d925
titus njuguna <titusnjuguna59@gmail.com>
2022-01-27 00:09:27 +0300

    Negative Power of a Number

---
+++ Neg_Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Neg_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber, powerNumber;
+
+            Console.Write("Enter base number :");
+            baseNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter exponent number :");
+            powerNumber = Convert.ToDouble(Console.ReadLine());
+
+            double returnNumber = Math.Pow(baseNumber, powerNumber);
+            Console.WriteLine($"{baseNumber}^{powerNumber} = {returnNumber}");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
