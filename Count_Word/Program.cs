d453f970dbf51409cd45d52ac4112e3f69d66899
titus njuguna <titusnjuguna59@gmail.com>
2022-01-27 11:47:18 +0300

    Count Words

---
+++ Count_Word/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Count_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Word;
+            Console.WriteLine("Start Typing...:");
+            Word = Console.ReadLine();
+            string[] words = Word.Split();
+            Console.WriteLine($"The sentence has {words.Length} words");
+        }
+    }
+}
