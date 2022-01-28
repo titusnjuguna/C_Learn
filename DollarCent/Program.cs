f9877864703cfce113ffa010b28bc8e709be2b90
titus njuguna <titusnjuguna59@gmail.com>
2022-01-28 16:07:23 +0300

    Dollar to cent convert

---
+++ DollarCent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DollarCent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Dollar,Cents;
+            
+            Console.WriteLine("Enter the Dollar amount:");
+            Dollar = Convert.ToDouble(Console.ReadLine());
+            Cents = Dollar * 100;
+            Console.WriteLine($"You had ${Dollar} and now you have {Cents} Cents");
+            Console.ReadKey();
+        }
+    }
+}
