ef6fa67f6a36be84943ab6059ce1bb566d74a853
titus njuguna <titusnjuguna59@gmail.com>
2022-01-29 09:26:15 +0300

    Odd or Even

---
+++ Odd_Even/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Odd_Even
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num%2==0){
+                Console.WriteLine($"{num} is a even number");
+            }
+            else{
+                Console.WriteLine($"{num} is an odd number");
+            }
+
+        
+
+            
+        }
+    }
+}
---
+++ fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static long[] numbers;
+        static long fib(int n){
+            if(0 == numbers[n]){
+                numbers[n] = fib(n-1) + fib(n-2);
+            }
+            return numbers[n];
+
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter number:");
+            int n = int.Parse(Console.ReadLine());
+            numbers = new long[n + 2];
+            numbers[1] = 1;
+            numbers[2] = 1;
+            long result = fib(n);
+            Console.WriteLine($"fibonacci where {n} is {result}");
+            Console.ReadLine();
+        }
+    }
+}
